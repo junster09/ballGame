a97bf7e2bc684c50d2ed1b5bed10aa52b74c3bc6
junster09 <ajr2z2025@gmail.com>
2020-01-31 21:35:51 -0800

    Made currency work

--- Scripts/BasicPlayer.lua
+++ Scripts/BasicPlayer.lua
@@ -2,29 +2,17 @@ local EQUIPMENT = script:GetCustomProperty("thisEQ"):WaitForObject()
 
 
 local thisPlayer
-local currency = 0
+--local currency = 0
 UI.SetCursorVisible(true)
 
 
 function OnEquipped(_,player)
-    thisPlayer = player
-    UpdateCurrencyToServer()
+    player:SetResource("currency",0)
 
 
 
 end
 
-function onGainCurrency(amount)
-    print("Event recieved")
-    currency = currency + amount
-    UpdateCurrencyToServer()
-end
 
-function UpdateCurrencyToServer()
-
-    thisPlayer.serverUserData.currency = currency
-    print(thisPlayer.serverUserData.currency)
-end
 
 EQUIPMENT.equippedEvent:Connect(OnEquipped)
-Events.Connect("GainCurrency",onGainCurrency,amount)
--- Scripts/JunkPickupScript.lua
+++ Scripts/JunkPickupScript.lua
@@ -3,15 +3,12 @@ local HIT_BOX = script:GetCustomProperty("HitBox"):WaitForObject()
 
 local currencyCount =  script.parent:GetCustomProperty("pickupCurrency")
 
-
---print(currencyCount)
-
-
 function OnBeginOverlap(trigger, other)
     print("sent to ",other.name)
+
     if other:IsA("Player") then
-        Events.BroadcastToPlayer(other,"GainCurrency",currencyCount)
-        --script.parent:Destroy()
+        other:AddResource("currency",currencyCount)
+        script.parent:Destroy()
     end
 end
 
