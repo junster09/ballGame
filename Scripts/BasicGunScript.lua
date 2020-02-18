4a3a57b49966d59ccb6f5e91c6df9d1c0e1ced63
junster09 <ajr2z2025@gmail.com>
2020-02-17 17:56:32 -0800

    Update BasicGunScript.lua
    added comments

--- Scripts/BasicGunScript.lua
+++ Scripts/BasicGunScript.lua
@@ -22,13 +22,13 @@ local EQUIPMENT = script:GetCustomProperty("Equipment"):WaitForObject()
 
 
 --VARS FROM ABILITY
-local MAX_HEAT = ABILITY:GetCustomProperty("MaxHeat")
-local DAMAGE = ABILITY:GetCustomProperty("Damage")
-local BULLET = ABILITY:GetCustomProperty("Bullet")
+local MAX_HEAT = ABILITY:GetCustomProperty("MaxHeat") --used for non-existant gun overheat mechanic
+local DAMAGE = ABILITY:GetCustomProperty("Damage") --damage
+local BULLET = ABILITY:GetCustomProperty("Bullet") 
 
 --GUN STUFF
-local BULLET_SPEED = ABILITY:GetCustomProperty("BulletSpeed")
-local BULLET_DRAG = ABILITY:GetCustomProperty("BulletDrag")
+local BULLET_SPEED = ABILITY:GetCustomProperty("BulletSpeed") 
+local BULLET_DRAG = ABILITY:GetCustomProperty("BulletDrag") --bullet deceleration, negatives make faster
 
 --local vars
 local isShooting = false
@@ -37,17 +37,16 @@ local thisPlayer = nil
 --gun logic
 local rpm = ABILITY.executePhaseSettings.duration
 rpm = 60.0 / rpm
---rpm = rpm/60.0
 
 --TODO 
 --Spool up time
 --figure out proper ability intergration? Maybe?
 
 function Tick(deltaTime)
-    if(thisPlayer ~= nil) then
+    if(thisPlayer ~= nil) then --needed so script don't crash
         if isShooting and thisPlayer.isDead == false then
             CreateBullet()
-            Task.Wait(rpm)
+            Task.Wait(rpm) --Task.Wait() for slightly better performance, also tick isn't used for anything else
         end
     end
 
@@ -58,28 +57,29 @@ end
 
 function CreateBullet()
     local e = Projectile.Spawn(BULLET,script:GetWorldPosition(),script:GetWorldRotation() * Vector3.FORWARD)
-    e.gravityScale = 0
+    e.gravityScale = 0 --nograv
     e.speed = BULLET_SPEED
     e.drag = BULLET_DRAG
-    e.capsuleLength = 0
-    e.impactEvent:Connect(OnBulletHit)
+    e.capsuleLength = 0 --change this number for longer hitboxes
+    e.impactEvent:Connect(OnBulletHit) --sets a listener for when the bullet hits
 end
 
-function OnBulletHit(proj,other,hitResult)
+function OnBulletHit(proj,other,hitResult) 
     if(other:IsA("Player")) then
 
         if(other ~= thisPlayer) then
-            local r = Damage.New(DAMAGE)
-            r.reason = DamageReason.COMBAT
-            r.sourceAbility = ABILITY
-            r.sourcePlayer = thisPlayer
-            other:ApplyDamage(r)
+            local r = Damage.New(DAMAGE) --create damage 
+            r.reason = DamageReason.COMBAT --give reason
+            r.sourceAbility = ABILITY --give ability refrence
+            r.sourcePlayer = thisPlayer --give player refrence
+            other:ApplyDamage(r) --atacc
         end
 
     end
 
 end
 
+--Sets player listeners when equipped
 function OnEquipped(eq,player)
     if(player == EQUIPMENT.owner) then
         player.bindingPressedEvent:Connect(OnButtonPress)
@@ -88,6 +88,7 @@ function OnEquipped(eq,player)
     end
 end
 
+--Same as OnEquipped, but made for when the item is given to the player instead
 function OnGiven(player)
     player.bindingPressedEvent:Connect(OnButtonPress)
     player.bindingReleasedEvent:Connect(OnButtonRelease)
@@ -95,7 +96,7 @@ function OnGiven(player)
 end
 
 
-
+--shooty shoot shoot
 function OnButtonPress(player,button)
     if(button == ABILITY.actionBinding and player.isDead == false) then
         --Events.BroadcastToPlayer(player,"_PlayerShooting")
@@ -103,6 +104,7 @@ function OnButtonPress(player,button)
     end
 end
 
+--finger off
 function OnButtonRelease(player,button)
     if(button == ABILITY.actionBinding) then
         --Events.BroadcastToPlayer(player,"_PlayerStopShooting")
@@ -110,6 +112,7 @@ function OnButtonRelease(player,button)
     end
 end
 
+--so script don't crash if its alone and sad
 if(EQUIPMENT ~= nil) then
     EQUIPMENT.equippedEvent:Connect(OnEquipped)
 end
